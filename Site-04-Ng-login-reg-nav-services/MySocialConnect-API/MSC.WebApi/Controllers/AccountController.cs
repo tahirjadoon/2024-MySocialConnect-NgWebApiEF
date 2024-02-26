2b6ab701c0573f1e2f06fb1d689ceea971008877
tahirjadoon <tjadoon@gmail.com>
2024-02-26 01:36:52 -0500

    Site-04-Ng-login-reg-nav-services initial

---
+++ Site-04-Ng-login-reg-nav-services/MySocialConnect-API/MSC.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MSC.Core.BusinessLogic;
+using MSC.Core.DB.Entities;
+using MSC.Core.Dtos;
+
+namespace MSC.WebApi.Controller;
+
+// /api/account
+public class AccountController : BaseApiController
+{
+    private readonly IUserBusinessLogic _userBusinessLogic;
+
+    public AccountController(IUserBusinessLogic userBusinessLogic)
+    {
+        _userBusinessLogic = userBusinessLogic;
+    }
+
+    [HttpPost("register")]
+    public async Task<ActionResult<LoggedInUserDto>> Register([FromBody] UserRegisterDto registerUser)
+    {
+        if(await _userBusinessLogic.UserExists(registerUser.UserName))
+            return BadRequest("Username already taken");
+
+        var loggedInUser = await _userBusinessLogic.RegisterUserAsync(registerUser);
+        if(loggedInUser == null || string.IsNullOrWhiteSpace(loggedInUser.UserName))
+            return BadRequest("Unable to create registration");
+
+        return loggedInUser;
+    }
+
+    [HttpPost("login")]
+    public async Task<ActionResult<LoggedInUserDto>> Login([FromBody] LoginDto login)
+    {
+        var user = await _userBusinessLogic.LoginAsync(login);
+        if (user == null || string.IsNullOrWhiteSpace(user.UserName) || string.IsNullOrWhiteSpace(user.Token))
+            return Unauthorized("Unable to login user");
+
+        return Ok(user);
+    }
+}
---
+++ Site-04-Ng-login-reg-nav-services/MySocialConnect-SPA/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  title: "MySocialConnect",
+  displayConsoleLog: false,
+  usebaseUrlHttps: true,
+  webApiBaseUrlHttps: 'https://localhost:5001/',
+  webApiBaseUrlHttp: 'http://localhost:5000/'
+};
